edc1e2ced2b4b1d4e278a2973b170107f83bad7f
Aboubnan <a.boubnan@outlook.com>
2025-10-31 10:39:49 +0100

    Add pg dependency for PostgreSQL on Render

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,17 @@
+// export default ({ env }) => ({
+//   host: env('HOST', '0.0.0.0'),
+//   port: env.int('PORT', 1337),
+//   app: {
+//     keys: env.array('APP_KEYS'),
+//   },
+// });
+
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('PUBLIC_URL', 'https://naya-web-agency-back.onrender.com'),
   app: {
     keys: env.array('APP_KEYS'),
   },
 });
+
